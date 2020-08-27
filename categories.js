003030854d6159c243f650f643c27cf455b94b27
br-monteiro <edson.monteiro@chaordicsystems.com>
2020-08-27 11:58:57 -0300

    feat: added input of categories

---
+++ categories.js
@@ -0,0 +1,25 @@
+module.exports = [{
+    id: 'futebol',
+    parent: null
+}, {
+    id: 'bola',
+    parent: 'futebol'
+}, {
+    id: 'meia',
+    parent: 'chuteira'
+}, {
+    id: 'chuteira',
+    parent: 'futebol'
+}, {
+    id: 'test',
+    parent: null
+}, {
+    id: 'outro',
+    parent: 'meia'
+}, {
+    id: 'manguito',
+    parent: 'voley'
+}, {
+    id: 'voley',
+    parent: null
+}]
