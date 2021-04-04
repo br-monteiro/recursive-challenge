72fcdf2c69e0019c3c9eee6cd185daa301914a63
Edinho Monteiro <bruno.monteirodg@gmail.com>
2021-04-04 12:36:14 -0300

    docs: adjustments on JSDocs

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const categories = require('./categories')
 
 /**
  * Build a tree of category based on array of categories
- * @param { Array <Category> } values - The array of categories
+ * @param { Array<Category> } values - The array of categories
  * @return { Object } - The tree of categories
  */
 function buildCategoriesTree (values) {
   /**
    * Buld a tree of category
-   * @param { Array <Category> } categories - The array of categories
+   * @param { Array<Category> } categories - The array of categories
    * @param { String | null } currentCategoryId - The parent of category
    * @return { CategoryTree }
    */
